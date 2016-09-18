3f5f4a4aa82d8b87b1b0dff38dc5b5301935ffc6
H992109898 <992109898@qq.com>
2016-09-18 20:36:25 +0800

    第一次更新
    .....学习github

---
+++ src/learn_SQL/test.java
@@ -0,0 +1,26 @@
+package learn_SQL;
+
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class test {
+
+	public static void main(String[] args) {
+		try {
+			new Driver();
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo",
+					"root", "root");
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+}
